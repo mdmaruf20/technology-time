d83379c6fa37dc7a147d7da2a3b2da7932d80cb2
mdmaruf <freelancermaruf20@gmail.com>
2020-08-04 22:50:15 +0600

    modified code

---
+++ client/src/components/TopBar/TopSocialIcon/TopSocialItem/TopSocialItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TopSocialItem extends Component {
+    render() {
+        return (
+            <li></li>
+        );
+    }
+}
+
+export default TopSocialItem;
